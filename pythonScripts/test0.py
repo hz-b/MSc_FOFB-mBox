739c1865bfee79ef3d1086dff1b147576c7a0bfd
Olivier CHURLAUD <olivier@churlaud.com>
2016-01-28 17:34:56 +0100

    1st script

---
+++ pythonScripts/test0.py
@@ -0,0 +1,14 @@
+i = 0
+
+def corr_value(BPMx, BPMy):
+    global i
+    i+=1
+
+    # rate = 150 ms
+    """
+        CMx then CMy. In this script we don't care about the BPM values
+        Loop over each CM, them over each frequency, then time. 
+        The only way to know where we are in the loop is i. + other global variables
+    """
+    
+    return CMx, CMy
