ad2e045f3d7085dc96ee8bbfcbd1c47ca516ca6e
Olivier CHURLAUD <olivier@churlaud.com>
2016-02-11 11:17:05 +0100

    first script python to test

--- pythonScripts/test0.py
+++ pythonScripts/test0.py
@@ -8,11 +8,12 @@ def corr_value(BPMx, BPMy, CMx_nb, CMy_nb):
     # rate = 150 ms
     """
         CMx then CMy. In this script we don't care about the BPM values
-        Loop over each CM, them over each frequency, then time. 
+        Loop over each CM, them over each frequency, then time.
         The only way to know where we are in the loop is i. + other global variables
     """
-
-    print(BPMx)
+    a = BPMx.size
+    b = BPMy.size
+    print('{} - {}, {}'.format(i,a,b))
     CMx = np.zeros(CMx_nb)+10
     CMx[20] = 123.45678910
     CMy = np.zeros(CMy_nb)+12
