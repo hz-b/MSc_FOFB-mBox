dae790394dbf6c83bbb8e107fa0bc0f6a2f1ff9c
Olivier CHURLAUD <olivier@churlaud.com>
2016-04-05 12:16:41 +0200

    messenger works well

--- src/logger/messenger.cpp
+++ src/logger/messenger.cpp
@@ -1,4 +1,8 @@
 #include "logger/messenger.h"
+#include "logger/logger.h"
+
+// Just so that it don't get sent by  error
+#define STOPPING_MESSAGE "STOP-ME-406812310648"
 
 Messenger::Messenger::Messenger(zmq::context_t& context)
     : m_serve(false)
@@ -10,9 +14,9 @@ Messenger::Messenger::Messenger(zmq::context_t& context)
 
 Messenger::Messenger::~Messenger()
 {
+    this->stopServing();
     m_socket->close();
     delete m_socket;
-    this->stopServing();
 }
 
 void Messenger::Messenger::servingLoop()
@@ -20,17 +24,23 @@ void Messenger::Messenger::servingLoop()
     while (m_serve) {
         zmq::message_t request;
         m_socket->recv(&request);
-        std::string key((char*)request.data());
+        std::string message((char*)request.data(), request.size());
         const void* content;
         int size;
-        if (key == "KEYLIST") {
+        if (message == "KEYLIST") {
             std::string s = m_map.keyList();
             content = static_cast<const void*>(s.data());
             size = s.size();
-        } else if (m_map.has(key)) {
-            content = static_cast<const void*>(m_map.get_raw(key));
-            size = m_map.get_sizeof(key);
+        } else if (message == STOPPING_MESSAGE) {
+            m_serve = false;
+            std::string s = "ACK";
+            content = static_cast<const void*>(s.data());
+            size = s.size();
+        } else if (m_map.has(message)) {
+            content = static_cast<const void*>(m_map.get_raw(message));
+            size = m_map.get_sizeof(message);
         } else {
+            std::cout << "error\n";
             std::string s = "ERROR";
             content = static_cast<const void*>(s.data());
             size = s.size();
@@ -41,16 +51,27 @@ void Messenger::Messenger::servingLoop()
 
 void Messenger::Messenger::startServing()
 {
-    std::cout << "Starting thread caller.\n";
+    Logger::log() << "Starting server thread." << Logger::flush;
     m_serve = true;
     m_serverThread = std::thread(&Messenger::Messenger::servingLoop, this);
-    m_serverThread.detach();
+    //m_serverThread.detach();
 }
 
 void Messenger::Messenger::stopServing()
 {
-    std::cout << "Stopping thread caller.\n";
-    m_serve = false;
+    Logger::log() << "Stopping server thread." << Logger::flush;
+    zmq::context_t tmp_context(1);
+    zmq::socket_t socket_stop(tmp_context, ZMQ_REQ);
+    socket_stop.connect("tcp://localhost:3334");
+    std::string s = STOPPING_MESSAGE;
+    const void* content = static_cast<const void*>(s.data());
+    int size = s.size();
+    socket_stop.send(content, size);
+    zmq::message_t request;
+    socket_stop.recv(&request);
+    socket_stop.close();
+    tmp_context.close();
+
     m_serverThread.join();
 }
 
