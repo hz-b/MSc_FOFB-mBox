c41de9376f6940043399e9437906b8271ae69dd1
Olivier CHURLAUD <olivier@churlaud.com>
2016-04-26 12:12:32 +0200

    documentation

--- src/adc.h
+++ src/adc.h
@@ -46,8 +46,17 @@ public:
 
     /**
      * @brief Access to an element of the buffer.
+     *
+     * @param id Index of the buffer element to return
+     * @return Buffer element
      */
     RFM2G_INT16 bufferAt(int id) const { if (id < m_buffer.size()) return m_buffer.at(id); };
+
+    /**
+     * @brief Return the full buffer vector.
+     *
+     * @return The buffer vector
+     */
     std::vector<RFM2G_INT16> buffer() const { return m_buffer; };
 
     double waveIndexXAt(int id) const { return m_waveIndexX.at(id); };
@@ -56,12 +65,36 @@ public:
     void setWaveIndexY(std::vector<double> vect) { m_waveIndexY = vect; };
 
 private:
+
+    /**
+     * @brief Procedure to get the authorization to read the RFM.
+     *
+     * @param eventInfo Information about the event
+     * @return Status of the function call
+     */
     RFM2G_STATUS waitForEvent(RFM2GEVENTINFO &eventInfo);
+
+    /**
+     * @brief Pointer to the DMA object.
+     */
     DMA *m_dma;
+
+    /**
+     * @brief Pointer to the RFMDriver object.
+     */
     RFMDriver *m_driver;
+
+    /**
+     * @brief Vector representing the data buffer
+     */
     std::vector<RFM2G_INT16> m_buffer;
+
     std::vector<double> m_waveIndexX;
     std::vector<double> m_waveIndexY;
+
+    /**
+     * @brief RFM node with which to communicate.
+     */
     int m_node;
 };
 
