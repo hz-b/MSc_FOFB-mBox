1e3719968235c1348d7f9eaa786925ecda17650e
Olivier CHURLAUD <olivier@churlaud.com>
2016-06-15 17:32:59 +0200

    update save/load scripts

--- python_tools/save_data.py
+++ python_tools/save_data.py
@@ -4,20 +4,26 @@
 from __future__ import division, print_function, unicode_literals
 
 import sys
+import datetime
+
 import numpy as np
 import matplotlib.pyplot as plt
+
 from zmq_client import ValuesSubscriber
 from PyML import PyML
+import search_kicks.tools as sktools
 
 SAMPLE_NB = 1
 
 if __name__=='__main__':
+    measure_date = datetime.datetime.now()
     if len(sys.argv) > 1:
         filename = str(sys.argv[1])
-        print(filename)
     else:
-        filename = 'savenow'
-
+        strtime = measure_date.isoformat('_'.encode('ascii'))
+        strtime = '-'.join(strtime.split(':'))
+        filename = 'dump-save-' + strtime
+    print("Save in {}".format(filename))
     pml = PyML()
     pml.setao(pml.loadFromExtern('PyML/bessyIIinit.py','ao'))
 
@@ -39,8 +45,12 @@ if __name__=='__main__':
     by = []
     cx = []
     cy = []
+    i = 0
     while 1:
         try:
+            i += 1
+            if i % (150*5) == 0:  # Every 5s 
+                print("Elapsed time = {}s".format(i/150))
             [valuesX, valuesY], loopPos = sBPM.receive(1)
             bx.append(valuesX[:,0].tolist())
             by.append(valuesY[:,0].tolist())
@@ -48,12 +58,17 @@ if __name__=='__main__':
             cx.append(valuesX[:,0].tolist())
             cy.append(valuesY[:,0].tolist())
         except KeyboardInterrupt:
-            d = dict()
-            d['BPM'] = {'x': np.array(bx).T, 'y': np.array(by).T}
-            d['CM'] = {'x': np.array(cx).T, 'y': np.array(cy).T}
-            np.save(filename,[d])
+            orbit = sktools.io.OrbitData(BPMx=np.array(bx).T,
+                                         BPMy=np.array(by).T,
+                                         CMx=np.array(cx).T,
+                                         CMy=np.array(cy).T,
+                                         names=None,
+                                         sampling_frequency=150,
+                                         measure_date=measure_date)
+            sktools.io.save_orbit_hdf5(filename, orbit)
             print('--------------------------------------')
             print('--------------DATA SAVED--------------')
             print('--------------------------------------')
+            sys.exit(0)
         except Exception:
             raise
